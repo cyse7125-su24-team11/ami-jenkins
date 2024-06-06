d14ee6b6a6e8aca40e1f4c0b4a9b31cdc3d00e9e
Shabina Singh <anibahs@gmail.com>
2024-06-06 15:46:18 -0400

    multi platform docker

--- jenkins-config/job.groovy
+++ jenkins-config/job.groovy
@@ -2,11 +2,11 @@ pipelineJob('seed') {
     description('Seed Pipeline Job')
     definition {
         cpsScm {
-            scriptPath('./jenkins-config/Jenkinsfile') // Reference the Jenkinsfile in your SCM
+            scriptPath('./Jenkinsfile') // Reference the Jenkinsfile in your SCM
             scm {
                 git {
                     remote {
-                        url('https://github.com/cyse7125-su24-team11/ami-jenkins.git')
+                        url('https://github.com/cyse7125-su24-team11/static-site.git')
                         credentials('GH_CRED') // Specify your GitHub credentials ID
                     }
                     branch('main') // Specify the branch you want to build
@@ -15,4 +15,3 @@ pipelineJob('seed') {
         }
     }
 }
-
